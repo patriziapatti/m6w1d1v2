a52834a26915a9d5f1d1972e1fa459e9b11f472e
patriziapatti <patrizia.patti5@gmail.com>
2024-08-15 15:35:36 +0100

    try catch e paginazione

--- backend/routes/authorRoutes.js
+++ backend/routes/authorRoutes.js
@@ -5,20 +5,52 @@ const router = express.Router()
 
 // get per richiamare tutti gli autori
 router.get("/", async (req,res)=>{
-    const allAuthors = await Author.find({})
-    res.send(allAuthors)
+    const page = req.query.page || 1
+    const perPage = req.query.perPage || 3
+    try {
+        const allAuthors = await Author.find({})
+        .sort({name:1, surname:1})
+        .skip((page-1)*perPage)
+        .limit(perPage)
+        const totalResults = await Author.countDocuments()// mi da il numero totale di documenti
+        const totalPages = Math.ceil(totalResults / perPage )  
+        // res.send(allAuthors)
+        res.send({
+            dati: allAuthors,
+            totalResults,
+            totalPages,
+            page,
+
+        })
+    } catch (error) {
+        res.status(404).send({message: 'Not Found'})
+    }
+    
 })
 //questa è la get per richiamare un singolo autore tramite id
 router.get("/:id", async (req,res)=>{
     const {id} =req.params
-    const author = await Author.findById(id)
-    res.send(author) 
+    try {
+        const author = await Author.findById(id)
+        res.status(200).send(author) 
+    } catch (error) {
+        res.status(404).send({message: 'Not Found'}) 
+    }
+    
 })
 
 router.post("/", async (req,res)=>{
     //crea un nuova istanza del modello autore con i dati definiti nella parentesi tonde (prendendoli dal body)
-    console.log(req.body)
+    // console.log(req.body)
     const author = new Author (req.body)
+    try {
+         //salva i dati prendendoli nel db , prendendoli dall'istanza
+        const newAuthor = await author.save()
+        //invia i dati al database
+        res.status(200).send(newAuthor)
+    } catch (error) {
+        res.status(400).send(error)
+    }
       //res.send("sono la post che crea un nuovo autore")
     //procedura estesa con campi statici:
     // const author = new Author ({
@@ -27,25 +59,29 @@ router.post("/", async (req,res)=>{
     //     email: req.body.email,
     //     avatar: req.body.avatar
     // })
-
-    //salva i dati prendendoli nel db , prendendoli dall'istanza
-    const newAuthor = await author.save()
-     //invia i dati al database
-    res.send(newAuthor)
-
 })
 
 router.put("/:id", async (req,res)=>{
     const {id} =req.params
-    const author = await Author.findByIdAndUpdate(id, req.body, {new:true}) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
-    await author.save();
-    // res.send(`sono la put e modifico l'autore con id ${id}`)
-    res.send(author)
+    try {
+        const author = await Author.findByIdAndUpdate(id, req.body, {new:true}) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
+        await author.save();
+        // res.send(`sono la put e modifico l'autore con id ${id}`)
+        res.status(200).send(author)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
 })
 
 router.delete("/:id", async (req,res)=>{
     const {id} =req.params
-    await Author.findByIdAndDelete(id)
-    res.send(`ho eliminato l'autore con id: ${id}`)
+    try {
+        await Author.findByIdAndDelete(id)
+        res.status(200).send(`ho eliminato l'autore con id: ${id}`)
+    } catch (error) {
+        res.status(404).send({message: `ID ${id} not found`})
+    }
+    
 })
 export default router
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,18 +1,43 @@
 import express, {json} from 'express';
 import Post from '../models/postSchema.js'
 
+
 const router = express.Router()
 
 // get per richiamare tutti i post
 router.get("/", async (req,res)=>{
-    const allPosts = await Post.find({})
-    res.send(allPosts)
+    const page = req.query.page || 1
+    const perPage = req.query.perPage || 3
+    try {
+        const allPosts = await Post.find({})
+        .sort({category:1, title:1})
+        .skip((page-1)*perPage)
+        .limit(perPage)
+
+        const totalResults = await Post.countDocuments()// mi da il numero totale di documenti
+        const totalPages = Math.ceil(totalResults / perPage ) 
+        res.send({
+            dati: allPosts,
+            page,
+            totalPages,
+            totalResults,
+        })
+        
+    } catch (error) {
+        res.status(404).send({message: 'Not Found'})
+    }
+    
 })
 //questa è la get per richiamare un singolo post tramite id
 router.get("/:id", async (req,res)=>{
-    const {id} =req.params
-    const post = await Post.findById(id)
-    res.send(post) 
+        const {id} =req.params
+        try {
+            const post = await Post.findById(id)
+            res.send(post) 
+        } catch (error) {
+            res.status(404).send({message: 'Not Found'})
+        }
+    
 })
 
 router.post("/", async (req,res)=>{
@@ -20,25 +45,40 @@ router.post("/", async (req,res)=>{
     // console.log(req.body)
     const post = new Post (req.body)
     //res.send("sono la post che crea un nuovo post")
-
-    //salva i dati prendendoli nel db , prendendoli dall'istanza
-    const newPost = await post.save()
-     //invia i dati al database
-    res.send(newPost)
+    try {
+        //salva i dati prendendoli nel db , prendendoli dall'istanza
+        const newPost = await post.save()
+        //invia i dati al database
+        res.send(newPost)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
 
 })
 
 router.put("/:id", async (req,res)=>{
     const {id} =req.params
-    const post = await Post.findByIdAndUpdate(id, req.body, {new:true}) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
-    await post.save();
-    // res.send(`sono la put e modifico il post con id ${id}`)
-    res.send(post)
+    try {
+        const post = await Post.findByIdAndUpdate(id, req.body, {new:true}) //new serve per restituire in author l'oggetto appena inserito, altrimenti non lo restituisce
+        await post.save();
+        // res.send(`sono la put e modifico il post con id ${id}`)
+        res.status(200).send(post)
+        
+    } catch (error) {
+        res.status(400).send(error)
+    }
+    
 })
 
 router.delete("/:id", async (req,res)=>{
     const {id} =req.params
-    await Post.findByIdAndDelete(id)
-    res.send(`ho eliminato il post con id: ${id}`)
+    try {
+        await Post.findByIdAndDelete(id)
+        res.status(200).send(`ho eliminato il post con id: ${id}`)
+    } catch (error) {
+        res.status(404).send({message: `ID ${id} not found`})
+    }
+    
 })
 export default router
